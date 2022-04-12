5623a7262c1ad9a731839ca41e691672134e5490
Kyle <kyle.weidner14@protonmail.com>
2022-04-11 20:06:20 -0600

    refactor

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x45FA61b7c20C9CB78bF2Ec0886c6DDa4297b0438'
+    '0x7f51a57e5ddF5a7686E9FD5C5984CC3b15985399'
 );
 
 export default instance;
