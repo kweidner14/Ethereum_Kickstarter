e2ffaea81089e076f03c614dfa5bb349e007f87a
Kyle <kyle.weidner14@protonmail.com>
2022-04-13 07:58:56 -0600

    create and configure components

---
+++ components/ContributeForm.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import {Button, Form, Input, Message} from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
+import {Router} from '../routes';
+
+class ContributeForm extends Component {
+    state = {
+        value: ''
+    };
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        const campaign = Campaign(this.props.address);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+
+            // Update screen automatically upon completion of contribution
+            Router.replaceRoute(`/campaigns/${this.props.address}`)
+        } catch (err) {
+
+        }
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.onSubmit}>
+                <Form.Field>
+                    <label>Amount to Contribute</label>
+                    <Input
+                        label='ether'
+                        labelPosition='right'
+                        value={this.state.value}
+                        onChange={event => this.setState({value: event.target.value})}
+                    />
+                </Form.Field>
+                <Button primary>Contribute!</Button>
+            </Form>
+        )
+    }
+}
+
+export default ContributeForm;
