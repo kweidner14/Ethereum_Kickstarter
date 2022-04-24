398d610bf0da4371b45205347f2830e615c5082a
Kyle <kyle.weidner14@protonmail.com>
2022-04-24 15:54:34 -0600

    create Contribute Form

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -6,13 +6,17 @@ import {Router} from '../routes';
 
 class ContributeForm extends Component {
     state = {
-        value: ''
+        value: '',
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
 
         const campaign = Campaign(this.props.address);
+
+        this.setState({loading: true, errorMessage: ''});
 
         try {
             const accounts = await web3.eth.getAccounts();
@@ -24,13 +28,16 @@ class ContributeForm extends Component {
             // Update screen automatically upon completion of contribution
             Router.replaceRoute(`/campaigns/${this.props.address}`)
         } catch (err) {
-
+            this.setState({errorMessage: err.message});
         }
+
+        this.setState({loading: false, value: ''})
+
     }
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label>Amount to Contribute</label>
                     <Input
@@ -40,7 +47,8 @@ class ContributeForm extends Component {
                         onChange={event => this.setState({value: event.target.value})}
                     />
                 </Form.Field>
-                <Button primary>Contribute!</Button>
+                <Message error header="Oops!" content={this.state.errorMessage} />
+                <Button primary loading={this.state.loading}>Contribute!</Button>
             </Form>
         )
     }
